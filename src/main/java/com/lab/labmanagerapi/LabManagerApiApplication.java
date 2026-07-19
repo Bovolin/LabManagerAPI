5a3715b613bd4735ce97738ad2bfa5a901245a5e
Bovolin <filipe.bovolin@gmail.com>
2026-07-18 23:10:08 -0300

    Initial commit

---
+++ src/main/java/com/lab/labmanagerapi/LabManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.lab.labmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabManagerApiApplication.class, args);
+    }
+
+}
