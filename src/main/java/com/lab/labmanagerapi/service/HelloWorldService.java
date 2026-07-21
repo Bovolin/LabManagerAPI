424dd4b7c1491b4ab53efb081c51cfcb5f640ed6
Filipe Bovolin <filipe.bovolin@gmail.com>
2026-07-21 20:23:56 -0300

    Teste de controller, configuration e service

---
+++ src/main/java/com/lab/labmanagerapi/service/HelloWorldService.java
@@ -0,0 +1,10 @@
+package com.lab.labmanagerapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloWorldService {
+    public String helloWorld(String name){
+        return "Hello " + name;
+    }
+}
